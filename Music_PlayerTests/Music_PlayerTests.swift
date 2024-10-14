8870108a361295ab3ade7efa0c92ea82be3b3c78
Atik hasan <atikh9979@gmail.com>
2024-10-14 19:42:11 +0600

    Initial Commit

---
+++ Music_PlayerTests/Music_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerTests.swift
+//  Music_PlayerTests
+//
+//  Created by Atik Hasan on 10/14/24.
+//
+
+import Testing
+@testable import Music_Player
+
+struct Music_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
